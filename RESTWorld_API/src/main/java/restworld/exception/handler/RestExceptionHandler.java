e2b27d42da0ea53e4a6488c2ef784a1c64e6544d
Michael Boren <mboren@cooksys.com>
2017-02-02 11:47:11 -0600

    Added exception handler & handling for Hibernate Transaction exceptions in case of validation violations

---
+++ RESTWorld_API/src/main/java/restworld/exception/handler/RestExceptionHandler.java
@@ -0,0 +1,58 @@
+package restworld.exception.handler;
+
+import javax.validation.ConstraintViolationException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.transaction.TransactionSystemException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class RestExceptionHandler extends ResponseEntityExceptionHandler {
+
+	private Logger log = LoggerFactory.getLogger(getClass());
+
+	public RestExceptionHandler() {
+		super();
+	}
+
+	@Override
+	protected final ResponseEntity<Object> handleHttpMessageNotReadable(final HttpMessageNotReadableException ex,
+			final HttpHeaders headers, final HttpStatus status, final WebRequest request) {
+		ResponseEntity<Object> errorResponse = handleExceptionInternal(ex, ex.getMessage(), headers, HttpStatus.BAD_REQUEST, request);
+		log.debug(errorResponse.toString());
+		return errorResponse;
+	}
+
+	@Override
+	protected final ResponseEntity<Object> handleMethodArgumentNotValid(final MethodArgumentNotValidException ex,
+			final HttpHeaders headers, final HttpStatus status, final WebRequest request) {
+		ResponseEntity<Object> errorResponse = handleExceptionInternal(ex, ex.getMessage(), headers, HttpStatus.BAD_REQUEST, request);
+		log.debug(errorResponse.toString());
+		return errorResponse;
+	}
+
+	@ExceptionHandler
+	public final ResponseEntity<Object> handleBadRequest(final Exception ex,
+			final WebRequest request) {
+		ResponseEntity<Object> errorResponse = handleExceptionInternal(ex, ex.getMessage(), new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
+		log.debug(errorResponse.toString());
+		return errorResponse;
+	}
+	
+    @ExceptionHandler(TransactionSystemException.class)
+    public ResponseEntity<Object> handleBadTransaction(final TransactionSystemException ex, final WebRequest request) {
+        ResponseEntity<Object> errorResponse = handleExceptionInternal(ex, ex.getRootCause().getMessage(), new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
+		log.debug(errorResponse.toString());
+		return errorResponse;
+    }
+}
